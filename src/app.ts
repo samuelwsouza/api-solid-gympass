7d56a8cc85b4ec80eef88238edd28fc7aac1c072
Samuel Souza <samukawsouza@gmail.com>
2026-09-15 20:30:04 -0300

    feat: error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import { fastify } from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => { 
+  if (error instanceof ZodError) {
+    return reply.status(400).send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // logar com uma ferramenta como DataLog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+ })
