f42b0a807dcd196a616e1a43516224f86478b92b
Samuel Souza <samukawsouza@gmail.com>
2026-09-20 20:38:10 -0300

    feat: tdd included with check-in

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -16,6 +16,12 @@ export class CheckInUseCase {
   ) {}
 
   async execute({ userId, gymId }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(userId, new Date())
+
+    if (checkInOnSameDay) {
+      throw new Error()
+    }
+
     const checkIn = await this.checkInsRepository.create({
       gym_id: gymId,
       user_id: userId,
